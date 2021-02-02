618e7bd4e3d76376f67fb50eac1c4be4b956ce0a
Mthabisi Ndlovu <26140169+codymndlovu@users.noreply.github.com>
2021-02-02 21:33:15 +0200

    Refactored code in implementation.py

--- implementation.py
+++ implementation.py
@@ -3,38 +3,44 @@
 # Thursday, 21 January 2021
 
 # 6
-def custom_trim(string):
-    new = string.strip(' ').split(' ')
-    new = list(filter(lambda x: x != '', new))
-    return ' '.join(new)
+def remove_extra_spaces(string):
+    return ' '.join(filter(lambda x: x != "", string.split()))
 
 # 7
-def find_occurence(string):
-    output_dict = {}
-    for el in string.lower():
-        if el not in output_dict.keys():
-            output_dict[el] = list(string.lower()).count(el)
-    return output_dict
+def count_chars(string):
+    char_counts = {}
+    for char in string:
+        if char not in char_counts:
+            char_counts[char] = 1
+        else:
+            char_counts[char] += 1
+    return char_counts
 
 # 8
 def nested_list_to_list(n_list):
     return list(set([el for lst in n_list for el in lst]))
 
 # 9
-def find_missing(list1, list2):
-    return list(set(list1) - set(list2))
-
+def find_missing(list_a, list_b):
+    """ Return an element in list_a that is not in list_b.
+    
+    list_b has all elements in list_a in the same order 
+    except one missing element.
+    """
+    for el in list_a:
+        if el not in list_b:
+            return el
 # 10
 def unpack_int_to_list(num):
     final = []
     for current in range(1, num + 1):
-        for _ in range(1, current + 1):
+        for _ in range(current):
             final.append(current)
     return final
 
 def main():
-    print(custom_trim("     Hello   there  I am here   "), "\n")
-    print(find_occurence("Hello there I am here"), "\n")
+    print(remove_extra_spaces("     Hello   there  I am here   "), "\n")
+    print(count_chars("Hello there I am here"), "\n")
     print(nested_list_to_list([[1, 2, 1], [0, 1, 3], [2, 3, 5]]), "\n")
     print(find_missing([1, 2, 3, 5], [1, 3, 5]), "\n")
     print(unpack_int_to_list(4))
